f946b9c5c2e350cc329bb3d3d6b52e33dcbe0515
Alvarashion <alvarotorrezc@gmail.com>
2024-03-07 14:58:33 -0400

    Cliente RESTful funcionando

--- 2024_02_28/client.py
+++ 2024_02_28/client.py
@@ -1,6 +1,12 @@
 import requests
 
 url="http://localhost:8000/"
+
+# Consulta por QUery params
+ruta_filtrar_nombres = url + "estudiantes?carrera=Ingienieria de Sistemas"
+filtrar_nombres_response = requests.request(method="GET", url=ruta_filtrar_nombres)
+print (filtrar_nombres_response.text)
+
 # GET Consulta a la lista estudiantes
 ruta_filtrar_carreras = url + "carreras"
 filtrar_carreras_response = requests.request(method="GET", url=ruta_filtrar_carreras)
@@ -9,7 +15,7 @@ print (filtrar_carreras_response.text)
 
 ruta_filtrar_estudiantes_carrera = url + "estudiantes/Arquitectura"
 filtrar_estudiantes_carrera_response = requests.request(method="GET", url=ruta_filtrar_estudiantes_carrera)
-print (filtrar_estudiantes_carrera_response.text)
+#print (filtrar_estudiantes_carrera_response.text)
 
 #POST Agregar estudiantes de Economia
 
@@ -29,10 +35,10 @@ nuevo_estudiante2= {
     "apellido":"Mamani",
     "carrera":"Economia"
 }
-print(post_response.text)
+#print(post_response.text)
 post_response= requests.request(
     method="POST",
     url=ruta_post,
     json=nuevo_estudiante
 )
-print(post_response.text)
+#print(post_response.text)
